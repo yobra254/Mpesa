18e454686080cc721438afb2d12e672da6e541ec
yobra254 <38029659+yobra254@users.noreply.github.com>
2022-10-07 15:20:25 +0300

    added heroku in allowed hosts

--- daraja/settings/production.py
+++ daraja/settings/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")                                       
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tranquil-bayou-13784.herokuapp.com']
 
 
 # Application definition
